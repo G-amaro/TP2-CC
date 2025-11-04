f0d1666fd65c5ff923c4ea97e9b0dc93aeb7f0e6
Gonçalo <a106803@alunos.uminho.pt>
2025-11-04 16:31:56 +0000

    sync feita

--- rover/sync_rover.py
+++ rover/sync_rover.py
@@ -2,6 +2,7 @@ import socket
 import json
 import sys
 import os
+import logging
 import time
 from datetime import datetime
 
@@ -11,32 +12,55 @@ if sys.argv.__len__() == 2:
     rover_id = int(sys.argv[1])
 
 file_dir = os.path.dirname(__file__)
-json_path = os.path.join(file_dir, "../info/rovers_info.json")
-with open(json_path, "r") as f:
-    dados = json.load(f)
+log_path = os.path.join(file_dir, "../logs/recorder.log")
 
-rover_info = None
+logging.basicConfig(
+    filename=log_path,
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
-for r in dados:
-    if r["id"] == rover_id:
-        rover_info = r
-        break
-
-
-
+MOTHER_IP = None
 MOTHER_PORT = 50000
-MOTHER_IP = '10.0.3.20'
+
+
+if rover_id == 3:
+    MOTHER_IP = '10.0.2.20'
+if rover_id == 1 or rover_id == 2:
+    MOTHER_IP = '10.0.3.20'
 
 msg_sync= {
-        "rover_id": str(rover_info["id"]),
-        "message_type": "sync",
-        "message": "ola, sou o rover " + str(rover_info["id"]),
-        "timestamp": datetime.now().isoformat()
+        "rover_id": str(rover_id),
+        "type": "sync request"
+        #,"timestamp": datetime.now().isoformat()
     }
 
-
+sending_max_times = 5
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(('0.0.0.0', 0))
 
-sock.sendto(json.dumps(msg_sync).encode('utf-8'), (MOTHER_IP, MOTHER_PORT))
+while sending_max_times > 0:
+
+    sock.sendto(json.dumps(msg_sync).encode('utf-8'), (MOTHER_IP, MOTHER_PORT))
+    sending_max_times-=1
+
+
+
+    sock.settimeout(3)
+    try:
+        data, addr = sock.recvfrom(1024)
+        print("rover 2 recebeu: {}".format(data))
+        data= json.loads(data.decode('utf-8'))
+        if data['type'] == "sync ack":
+            logging.info(f"Nave Mae({addr[0]}:{addr[1]}) -> Rover {data['id']}  : {data['type']}")
+            break
+
+
+    except socket.timeout:
+        continue
+
+
+if sending_max_times == 0:
+    logging.error(f"Rover "+ str(rover_id) + ": Max number of sync tries exceeded. Sync ACK missing.")
 sock.close()
